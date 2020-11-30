b4dd8cc5f4279acd212913fe2010593c9533f168
anjaliryerabati <51506459+anjaliryerabati@users.noreply.github.com>
2020-11-30 23:14:59 +0530

    Add files via upload

---
+++ Ambusi_Project.ino
@@ -0,0 +1,379 @@
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+//SoftwareSerial Serial1(18,19);
+
+int  NIR1     = 22;
+int  NIR2     = 24;
+int  NIR3     = 26;
+int  NRED     = 28;
+int  NYELLOW  = 30;
+int  NGREEN   = 32;
+
+
+int  EIR1 = 23;
+int  EIR2 = 25;
+int  EIR3 = 27;
+int  ERED     = 29;
+int  EYELLOW  = 31;
+int  EGREEN   = 33;
+
+
+int  WIR1 = 48;
+int  WIR2 = 50;
+int  WIR3 = 52;
+int  WRED     = 42;
+int  WYELLOW  = 44;
+int  WGREEN   = 46;
+
+
+int  SIR1 = 49;
+int  SIR2 = 51;
+int  SIR3 = 53;
+int  SRED     = 43;
+int  SYELLOW  = 45;
+int  SGREEN   = 47;
+
+
+int ncnt=0,ecnt=0,scnt=0,wcnt=0;
+
+char Card_id[14];
+int i=0;
+bool stringComplete = false;  // whether the string Read is complete
+
+
+//LCD pin to Arduino
+const int pin_RS = 8; 
+const int pin_EN = 9; 
+const int pin_d4 = 4; 
+const int pin_d5 = 5; 
+const int pin_d6 = 6; 
+const int pin_d7 = 7; 
+const int pin_BL = 10; 
+LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
+void setup() {
+  pinMode(NIR1,INPUT_PULLUP);
+  pinMode(NIR2,INPUT_PULLUP);
+  pinMode(NIR3,INPUT_PULLUP);
+  pinMode(EIR1,INPUT_PULLUP);
+  pinMode(EIR2,INPUT_PULLUP);
+  pinMode(EIR3,INPUT_PULLUP);
+  pinMode(WIR1,INPUT_PULLUP);
+  pinMode(WIR2,INPUT_PULLUP);
+  pinMode(WIR3,INPUT_PULLUP);
+  pinMode(SIR1,INPUT_PULLUP);
+  pinMode(SIR2,INPUT_PULLUP);
+  pinMode(SIR3,INPUT_PULLUP);
+  pinMode(NRED,OUTPUT);
+  pinMode(NYELLOW,OUTPUT);
+  pinMode(NGREEN,OUTPUT);
+  pinMode(ERED,OUTPUT);
+  pinMode(EYELLOW,OUTPUT);
+  pinMode(EGREEN,OUTPUT);
+  pinMode(WRED,OUTPUT);
+  pinMode(WYELLOW,OUTPUT);
+  pinMode(WGREEN,OUTPUT);
+  pinMode(SRED,OUTPUT);
+  pinMode(SYELLOW,OUTPUT);
+  pinMode(SGREEN,OUTPUT);
+  digitalWrite(NRED,HIGH);
+  digitalWrite(NYELLOW,HIGH);
+  digitalWrite(NGREEN,HIGH);
+  digitalWrite(ERED,LOW);
+  digitalWrite(EYELLOW,LOW);
+  digitalWrite(EGREEN,LOW);
+  digitalWrite(WRED,LOW);
+  digitalWrite(WYELLOW,LOW);
+  digitalWrite(WGREEN,LOW);
+  digitalWrite(SRED,LOW);
+  digitalWrite(SYELLOW,LOW);
+  digitalWrite(SGREEN,LOW);
+  Serial1.begin(115200);
+  Serial.begin(9600);
+ lcd.begin(16, 2);
+ lcd.setCursor(3,0);
+ lcd.print("Welcome to ");
+ lcd.setCursor(5,1);
+ lcd.print("AMBUSI");
+}
+void loop() {
+  
+              if(!digitalRead(NIR1)&&digitalRead(NIR2)&&digitalRead(NIR3))
+              {
+                ncnt=1;
+              }
+              else if(!digitalRead(NIR1)&&!digitalRead(NIR2)&&digitalRead(NIR3))
+              {
+                ncnt=2;
+              }
+              else if(!digitalRead(NIR1)&&!digitalRead(NIR2)&&!digitalRead(NIR3))
+              {
+                ncnt=3;
+              }
+              else
+                ncnt=0;
+
+            if(!digitalRead(EIR1)&&digitalRead(EIR2)&&digitalRead(EIR3))
+              {
+                ecnt=1;
+              }
+              else if(!digitalRead(EIR1)&&!digitalRead(EIR2)&&digitalRead(EIR3))
+              {
+                ecnt=2;
+              }
+              else if(!digitalRead(EIR1)&&!digitalRead(EIR2)&&!digitalRead(EIR3))
+              {
+                ecnt=3;
+              }
+              else
+                ecnt=0;
+
+              if(!digitalRead(SIR1)&&digitalRead(SIR2)&&digitalRead(SIR3))
+              {
+                scnt=1;
+              }
+              else if(!digitalRead(SIR1)&&!digitalRead(SIR2)&&digitalRead(SIR3))
+              {
+                scnt=2;
+              }
+              else if(!digitalRead(SIR1)&&!digitalRead(SIR2)&&!digitalRead(SIR3))
+              {
+                scnt=3;
+              }
+              else
+                scnt=0;
+
+              if(!digitalRead(WIR1)&&digitalRead(WIR2)&&digitalRead(WIR3))
+              {
+                wcnt=1;
+              }
+              else if(!digitalRead(WIR1)&&!digitalRead(WIR2)&&digitalRead(WIR3))
+              {
+                wcnt=2;
+              }
+              else if(!digitalRead(WIR1)&&!digitalRead(WIR2)&&!digitalRead(WIR3))
+              {
+                wcnt=3;
+              }
+              else
+                wcnt=0;
+              if (stringComplete) 
+             {
+              i=0;
+              lcd.clear();
+              lcd.print("Amblnce Detected");
+              lcd.setCursor(0,1);
+              lcd.print("            ");
+              digitalWrite(NRED,HIGH);
+              digitalWrite(NYELLOW,LOW);
+              digitalWrite(NGREEN,HIGH);
+              digitalWrite(ERED,LOW);
+              digitalWrite(EYELLOW,LOW);
+              digitalWrite(EGREEN,HIGH);
+              digitalWrite(WRED,LOW);
+              digitalWrite(WYELLOW,HIGH);
+              digitalWrite(WGREEN,LOW);
+              digitalWrite(SRED,LOW);
+              digitalWrite(SYELLOW,HIGH);
+              digitalWrite(SGREEN,LOW);
+              delay(1000);
+              digitalWrite(NRED,LOW);
+                  digitalWrite(NYELLOW,HIGH);
+                  digitalWrite(NGREEN,HIGH);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,LOW);
+                  digitalWrite(WGREEN,HIGH);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,LOW);
+                  digitalWrite(SGREEN,HIGH);
+              delay(2000);
+              //memset(Card_id,'\0',11);
+              stringComplete = false;
+              delay(3000);
+            }
+          else
+        {
+                if(ncnt>ecnt&&ncnt>scnt&&ncnt>wcnt)
+                {
+                  lcd.clear();
+                  lcd.print("Traffic in North");
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,LOW);
+                  digitalWrite(NGREEN,HIGH);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,HIGH);
+                  digitalWrite(WGREEN,LOW);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,HIGH);
+                  digitalWrite(SGREEN,LOW);
+                  delay(1000);
+                  digitalWrite(NRED,LOW);
+                  digitalWrite(NYELLOW,HIGH);
+                  digitalWrite(NGREEN,HIGH);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,LOW);
+                  digitalWrite(WGREEN,HIGH);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,LOW);
+                  digitalWrite(SGREEN,HIGH);
+                  delay(5000);
+                }
+                else if(ecnt>ncnt&&ecnt>scnt&&ecnt>wcnt)
+                {
+                  lcd.clear();
+                  lcd.print("Traffic in East");
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,LOW);
+                  digitalWrite(NGREEN,HIGH);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,HIGH);
+                  digitalWrite(WGREEN,LOW);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,HIGH);
+                  digitalWrite(SGREEN,LOW);
+                  delay(1000);
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,HIGH);
+                  digitalWrite(NGREEN,LOW);
+                  digitalWrite(ERED,HIGH);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,LOW);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,LOW);
+                  digitalWrite(WGREEN,HIGH);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,LOW);
+                  digitalWrite(SGREEN,HIGH);
+                  delay(5000);
+                }
+                else if(scnt>ncnt&&scnt>ecnt&&scnt>wcnt)
+                {
+                  lcd.clear();
+                  lcd.print("Traffic in South");
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,LOW);
+                  digitalWrite(NGREEN,HIGH);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,HIGH);
+                  digitalWrite(WGREEN,LOW);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,HIGH);
+                  digitalWrite(SGREEN,LOW);
+                  delay(1000);
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,HIGH);
+                  digitalWrite(NGREEN,LOW);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,LOW);
+                  digitalWrite(WGREEN,HIGH);
+                  digitalWrite(SRED,HIGH);
+                  digitalWrite(SYELLOW,LOW);
+                  digitalWrite(SGREEN,LOW);
+                  delay(5000);
+                }
+                else if(wcnt>ncnt&&wcnt>ecnt&&wcnt>scnt)
+                {
+                  lcd.clear();
+                  lcd.print("Traffic in West");
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,LOW);
+                  digitalWrite(NGREEN,HIGH);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,HIGH);
+                  digitalWrite(WGREEN,LOW);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,HIGH);
+                  digitalWrite(SGREEN,LOW);
+                  delay(1000);
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,HIGH);
+                  digitalWrite(NGREEN,LOW);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,HIGH);
+                  digitalWrite(WRED,HIGH);
+                  digitalWrite(WYELLOW,LOW);
+                  digitalWrite(WGREEN,LOW);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,LOW);
+                  digitalWrite(SGREEN,HIGH);
+                  delay(5000);
+                } 
+                else
+                {
+                  lcd.clear();
+                  lcd.print("Traffic Yet to ");
+                  lcd.setCursor(0,1);
+                  lcd.print("  be Detected ");
+                  digitalWrite(NRED,HIGH);
+                  digitalWrite(NYELLOW,HIGH);
+                  digitalWrite(NGREEN,HIGH);
+                  digitalWrite(ERED,LOW);
+                  digitalWrite(EYELLOW,LOW);
+                  digitalWrite(EGREEN,LOW);
+                  digitalWrite(WRED,LOW);
+                  digitalWrite(WYELLOW,LOW);
+                  digitalWrite(WGREEN,LOW);
+                  digitalWrite(SRED,LOW);
+                  digitalWrite(SYELLOW,LOW);
+                  digitalWrite(SGREEN,LOW);
+                  delay(1000);
+                  
+                }
+                Serial.println("  ");
+                Serial.print("*");
+                Serial.print(ncnt,DEC);
+                Serial.print(",");
+                Serial.print(ecnt,DEC);
+                Serial.print(",");
+                Serial.print(wcnt,DEC);
+                Serial.print(",");
+                Serial.print(scnt,DEC);
+                Serial.print("#");
+                Serial.println("  ");
+                
+  }
+} 
+
+
+
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    Card_id[i] = inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (i == 12) {
+      Card_id[++i]='\0';
+      stringComplete = true;
+      i=0;
+    }
+    else
+    i++;
+  }
+}
+
---
+++ Traffic.ino
@@ -0,0 +1,141 @@
+
+// Hardware: NodeMCU
+
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+
+
+String apiKey = "UITOVF4DP3DC4FBJ";     //  Enter your Write API key from ThingSpeak
+
+const char *ssid =  "SIGMASOLUTION";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "asdfgfgf@1";
+const char* server = "api.thingspeak.com";
+
+char strl[5][10]={0};
+char snames[5][13]={"North :","East :","South :","West :"};
+char flag=0;
+char str[30]={'\0'};
+
+WiFiClient client;
+
+int readSerial(char result[]) 
+{
+  int i = 0;
+  char inChar;
+  do
+   {
+    while (Serial.available() > 0) 
+    {
+       inChar = Serial.read();
+      if (inChar == '#') 
+         {
+          result[i] = '\0';
+          Serial.flush();
+          return 0;
+         }
+      else// (inChar != '\r') 
+         {
+          result[i] = inChar;
+          i++;
+         }
+    }
+  }while(inChar!='#');
+}
+
+void setup() 
+{
+       Serial.begin(9600);
+       delay(10);
+       ESP.wdtDisable();
+       Serial.println("Connecting to ");
+       Serial.println(ssid);
+ 
+ 
+       WiFi.begin(ssid, pass);
+ 
+      while (WiFi.status() != WL_CONNECTED) 
+     {
+            delay(500);
+            Serial.print(".");
+     }
+      Serial.println("");
+      Serial.println("WiFi connected");
+}
+ 
+void loop() 
+{      
+                    Serial.println("Waiting...");
+                    char rcv;
+                      do{
+                       rcv = Serial.read();
+                      }while(rcv != '*');
+                       readSerial(str);
+                       Serial.println("Data Recieved...");
+                       Serial.println(str);
+                    int i;
+                    int j=0;
+                    int k=0;
+                    for(i=0;str[i]!='\0';i++)
+                    {
+                      if(str[i]!=',')
+                      {
+                        strl[j][k]=str[i];
+                        k++;
+                      }
+                      else
+                      {
+                        j++;
+                        k=0;
+                      }
+                    }          
+                   Serial.print("*");
+                  for(int i=0;i<4;i++)
+                    {
+                    Serial.print(snames[i]);
+                    Serial.print(strl[i]);
+                    Serial.print(", ");
+                    }
+                    Serial.println("*");
+ 
+                         if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+                      {                              
+                             String postStr = apiKey;
+                             postStr +="&field1=";
+                             postStr += strl[0];
+                             postStr +="&field2=";
+                             postStr += strl[1];
+                             postStr +="&field3=";
+                             postStr += strl[2];
+                             postStr +="&field4=";
+                             postStr += strl[3];
+//                             postStr +="&field5=";
+//                             postStr += strl[4];
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                            
+                             
+                        }
+                        flag=0;
+                   client.stop();
+                   Serial.println("Updated...");
+             
+          
+  delay(1000);
+}
+
+
+
+
+
+
+
